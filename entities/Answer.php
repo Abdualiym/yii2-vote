99b052bdba20a175c6976deaacb21c2d61239f86
aripdjanov <a.abdualiym@gmail.com>
2018-03-09 19:09:21 +0500

    fix

--- entities/Answer.php
+++ entities/Answer.php
@@ -2,7 +2,7 @@
 
 namespace abdualiym\vote\entities;
 
-use backend\entity\User;
+use backend\entities\User;
 use abdualiym\vote\entities\queries\VotesQuery;
 use lhs\Yii2SaveRelationsBehavior\SaveRelationsBehavior;
 use yii\behaviors\BlameableBehavior;
