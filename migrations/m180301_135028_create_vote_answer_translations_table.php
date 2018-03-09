7fca77a665b6de9b8ea94b63bc01efce4b3331ae
Achilov Davronbek <achilov21@yandex.com>
2018-03-09 11:18:31 +0500

    Update m180301_135028_create_vote_answer_translations_table.php

--- migrations/m180301_135028_create_vote_answer_translations_table.php
+++ migrations/m180301_135028_create_vote_answer_translations_table.php
@@ -18,7 +18,7 @@ class m180301_135028_create_vote_answer_translations_table extends Migration
             'lang_id' => $this->integer()->notNull(),
             'answer' => $this->text()->notNull()
         ]);
-        $this->addForeignKey('fk-vote_answer_questions-vote_answers_id', 'vote_answer_questions', 'answer_id', 'vote_answers', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk-vote_answer_questions-vote_answers_id', 'vote_answer_translations', 'answer_id', 'vote_answers', 'id', 'CASCADE', 'CASCADE');
     }
 
     /**
