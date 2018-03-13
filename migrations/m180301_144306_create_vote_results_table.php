8b29e73689de0c8abfd09ee2f37b5bab42b16c2a
Achilov Davronbek <achilov21@yandex.com>
2018-03-13 11:09:48 +0500

    migration utf

--- migrations/m180301_144306_create_vote_results_table.php
+++ migrations/m180301_144306_create_vote_results_table.php
@@ -12,7 +12,7 @@ class m180301_144306_create_vote_results_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('vote_results', [
+        $this->createTable('vote_answer_translations', [
             'id' => $this->primaryKey(),
             'answer_id' => $this->integer()->notNull(),
             'question_id' => $this->integer()->notNull(),
@@ -20,7 +20,7 @@ class m180301_144306_create_vote_results_table extends Migration
             'user_id'=> $this->integer()->defaultValue(null),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
-        ]);
+        ],'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
         $this->addForeignKey('fk-votes_results-vote_answers_id', 'vote_results', 'answer_id', 'vote_answers', 'id', 'CASCADE', 'CASCADE');
         $this->addForeignKey('fk_vote_results_vote_questions_id', 'vote_results', 'question_id', 'vote_questions', 'id', 'CASCADE', 'CASCADE');
     }
